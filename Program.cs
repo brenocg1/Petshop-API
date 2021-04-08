6543e9c35aca68cc577d227c0cfc21445d8e7624
BrenoCampos <brenocg@alu.ufc.br>
2021-04-08 18:30:12 -0300

    configure default port to run

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace petshop
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseUrls("http://localhost:5000/");
                 });
     }
 }
